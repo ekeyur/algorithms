932c5929367e298d0e76104417174323a822beb6
Keyur Patel <ekeyur@gmail.com>
2017-09-30 01:26:52 -0400

    comleted stringCompression

--- stringCompression.js
+++ stringCompression.js
@@ -1,20 +1,51 @@
 function compressString(s) {
-    var compressedString = '';
-    var counter = 1;
     var l = s.length;
     var i = 1;
-    //AAAAB
-    while(i<l) {
-        if (s[i] === s[i-1]){
-            counter++;
+    var cnt = 1;
+    var compressedString = '';
+    while(i < l) {
+        if(s[i] === s[i-1]){
+            cnt += 1;
         } else {
-            compressedString = compressedString + s[i-1] + String(counter);
-            counter = 1;
+            const temp_str = s[i-1] + String(cnt);
+            compressedString += temp_str;
+            cnt = 1;
         }
         i++;
     }
-    compressedString = compressedString + s[i-1] + String(counter);
+    const temp_str = s[i-1] + String(cnt);
+    compressedString += temp_str;
+
     return compressedString;
 }
 
-console.log(compressString('AABcc'));
+
+
+
+
+
+
+
+
+
+
+// function compressString(s) {
+//     var compressedString = '';
+//     var counter = 1;
+//     var l = s.length;
+//     var i = 1;
+//     //AAAAB
+//     while(i<l) {
+//         if (s[i] === s[i-1]){
+//             counter++;
+//         } else {
+//             compressedString = compressedString + s[i-1] + String(counter);
+//             counter = 1;
+//         }
+//         i++;
+//     }
+//     compressedString = compressedString + s[i-1] + String(counter);
+//     return compressedString;
+// }
+
+console.log(compressString('AAABcc'));
