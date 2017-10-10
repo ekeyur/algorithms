3f348652b3e62f920d772624f9244b176445badb
Keyur Patel <ekeyur@gmail.com>
2017-10-09 20:22:19 -0400

    changed logic in maxProfit sum

--- stockProfit.js
+++ stockProfit.js
@@ -1,5 +1,4 @@
 // Given an array of daily stock value. Make of suggest a transaction that would yield the highest profit and return the highest profit value. You have to buy before sell
-//
 // First make the array of prices: [14,34,12,20,43,22].
 // There could be a situation where the stock price decline everyday: [52,43,34,23,15,10].
 // There might be some edge cases
